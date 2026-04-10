a553d0c0763318e115edbeae4e87815da63a54fe
Lavanshi258 <lavanshigupta71@gmail.com>
2026-04-10 15:20:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,34 +2,12 @@ import streamlit as st
 import numpy as np
 import pickle
 
-model = pickle.load(open('loan_model.pkl', 'rb'))
+model = pickle.load(open('salary_model.pkl', 'rb'))
 
-st.title("Loan Eligibility Predictor")
+st.title("Salary Prediction App 💼")
 
-gender = st.selectbox("Gender", ["Male", "Female"])
-married = st.selectbox("Married", ["Yes", "No"])
-education = st.selectbox("Education", ["Graduate", "Not Graduate"])
-self_employed = st.selectbox("Self Employed", ["Yes", "No"])
+experience = st.number_input("Enter Years of Experience", min_value=0.0)
 
-app_income = st.number_input("Applicant Income")
-co_income = st.number_input("Coapplicant Income")
-loan_amount = st.number_input("Loan Amount")
-loan_term = st.number_input("Loan Term")
-credit_history = st.selectbox("Credit History", [1, 0])
-
-gender = 1 if gender == "Male" else 0
-married = 1 if married == "Yes" else 0
-education = 1 if education == "Graduate" else 0
-self_employed = 1 if self_employed == "Yes" else 0
-
-if st.button("Check Eligibility"):
-    data = np.array([[gender, married, education, self_employed,
-                      app_income, co_income, loan_amount,
-                      loan_term, credit_history]])
-
-    result = model.predict(data)
-
-    if result[0] == 1:
-        st.success("Loan Approved ✅")
-    else:
-        st.error("Loan Not Approved ❌")
+if st.button("Predict Salary"):
+    prediction = model.predict([[experience]])
+    st.success(f"Predicted Salary: ₹ {int(prediction[0])}")
--- train_model.py
+++ train_model.py
@@ -1,23 +1,18 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 import pickle
 
-df = pd.read_csv("loan_data.csv")
+# Load data
+df = pd.read_csv("salary_data.csv")
 
-df['Gender'] = df['Gender'].map({'Male':1, 'Female':0})
-df['Married'] = df['Married'].map({'Yes':1, 'No':0})
-df['Education'] = df['Education'].map({'Graduate':1, 'Not Graduate':0})
-df['Self_Employed'] = df['Self_Employed'].map({'Yes':1, 'No':0})
+X = df[['YearsExperience']]
+y = df['Salary']
 
-X = df[['Gender','Married','Education','Self_Employed',
-        'ApplicantIncome','CoapplicantIncome',
-        'LoanAmount','Loan_Amount_Term','Credit_History']]
-
-y = df['Loan_Status'].map({'Y':1,'N':0})
-
-model = LogisticRegression()
+# Train model
+model = LinearRegression()
 model.fit(X, y)
 
-pickle.dump(model, open('loan_model.pkl', 'wb'))
+# Save model
+pickle.dump(model, open('salary_model.pkl', 'wb'))
 
-print("Model trained and saved!")
+print("Model trained successfully!")
